b63fae9dff6b8cefb8d8bee1510c386425a59f34
poodhiambo3906 <poodhiambo3906@gmail.com>
2020-09-11 16:54:21 -0400

    Adding Project3

---
+++ Project3/app/data.py
@@ -0,0 +1,26 @@
+def CsvFiles():
+    csv_files = [
+        {
+            'id': 1,
+            'filename':'File One',
+            'original_file_name':'filename.csv',
+            'author':'Yemi John',
+            'create_date':'06-09-2020'
+        },
+         {
+            'id': 2,
+            'filename':'File Two',
+            'original_file_name':'filename2.csv',
+            'author':'Ade Josh',
+            'create_date':'05-09-2020'
+        },
+         {
+            'id': 3,
+            'filename':'File Three',
+            'original_file_name':'filename3.csv',
+            'author':'Femi King',
+            'create_date':'04-09-2020'
+        }
+    ]
+
+    return csv_files
